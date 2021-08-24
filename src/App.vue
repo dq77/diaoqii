27129ac65ee88ec468c500d7c31e722d7ef14200
掉漆 <madongmei@88.com>
2021-08-24 16:34:17 +0800

    样式更新

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,26 @@
   <router-view/>
 </template>
 
-<style>
-#app {
-  min-height: 100vh;
+<style lang='scss'>
+html {
+  max-width: 750px;
+  margin: 0 auto;
+}
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #666;
-  background-color: #f5f5f9;
   display: flex;
   flex-direction: column;
+  color: #666;
+  background-color: #f5f5f9;
   font-size: 0.28rem;
+  min-height: 100vh;
+}
+#app {
+  flex: 1 0 auto;
+  display: flex;
+  flex-direction: column;
 }
 #app > div {
   flex: 1 0 auto;
